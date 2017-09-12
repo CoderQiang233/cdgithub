a9465528956fb4fdd2571526fe1bc48c1bdd09b1
Wang Qiang <296361319@qq.com>
2017-09-12 18:32:30 +0800

    登录页搭建

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import { connect } from 'dva';
-import styles from './Login.css';
+import {connect} from 'dva';
+import { Row, Col } from 'antd';
+import classnames from 'classnames'
+import styles from './Login.less';
+import SLoginForm from '../components/login/SLoginForm';
+import TLoginForm from '../components/login/TLoginForm';
 
-function Login() {
-  return (
-    <div className={styles.normal}>
-      Route Component: Login
-    </div>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginForm:1
+    };
+  }
+
+  chooseLogin=(value)=>{
+    this.setState({loginForm:value});
+
+    console.log(this.state.props)
+  }
+
+
+  render() {
+
+
+    
+    return ( 
+      <div className = {styles.wrap} >
+        <Row type="flex" justify="space-around" align="middle" >
+        <Col span={10}>
+         <div className={styles.loginBox}>
+           <div className={styles.logo}>
+             <img src={require('../assets/images/logo.png')}/>
+           </div>
+           <Row gutter={32}>
+             <Col span={11}>
+               <div className={styles.loginForm}>
+                 <h1 className={styles.title}>统一身份验证</h1>
+                 <Row>
+                   <Col span={8}>
+                     <a className={classnames(styles.chooseLogin,{[styles.active]:this.state.loginForm==1})} value='1' onClick={this.chooseLogin.bind(this,1)}>学生登录</a>
+                   </Col>
+                   <Col span={8}>
+                     <a className={classnames(styles.chooseLogin,{[styles.active]:this.state.loginForm==2})} value='2' onClick={this.chooseLogin.bind(this,2)}>教师登录</a>
+                   </Col>
+                 </Row>
+                 {
+                   this.state.loginForm==1&&
+                   <SLoginForm></SLoginForm>
+                 }
+                 {
+                  this.state.loginForm==2&&
+                   <TLoginForm></TLoginForm>
+                 }
+               </div>
+             </Col>
+             <Col span={13}>
+               <div className={styles.infoBox}>
+                 111111
+               </div>
+             </Col>
+           </Row>
+         </div>
+        </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps() {
--- src/routes/Service.js
+++ src/routes/Service.js
@@ -9,6 +9,81 @@ import Servicelist from '../components/Service/Servicelist';
   //标签
 const CheckableTag = Tag.CheckableTag;
 const tagsFromServer = ['人事部', '保卫处', '大学生就业管理处', '行政处'];
+
+const arr=[
+    {
+        id:1,
+        imgpath:require('../assets/images/service1.png'),
+        name:'人事处',
+        bottom:'显示底部内容'
+  },
+  {
+      id:2,
+      imgpath:require('../assets/images/service2.png'),
+      name:'保卫处',
+      bottom:'显示底部内容显示底部内容显示底部内容显示底部内容'
+  },
+  {
+      id:3,
+      imgpath:require('../assets/images/service3.png'),
+      name:'就业管理处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:4,
+      imgpath:require('../assets/images/service4.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:5,
+      imgpath:require('../assets/images/service5.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:6,
+      imgpath:require('../assets/images/service1.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  }
+  ,  {
+      id:7,
+      imgpath:require('../assets/images/service2.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:8,
+      imgpath:require('../assets/images/service3.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:9,
+      imgpath:require('../assets/images/service4.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:10,
+      imgpath:require('../assets/images/service5.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  }
+  ,  {
+      id:11,
+      imgpath:require('../assets/images/service1.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  },
+  {
+      id:12,
+      imgpath:require('../assets/images/service2.png'),
+      name:'行政处',
+      bottom:'显示底部内容'
+  }
+]
 class Service extends React.Component{
   constructor(props){
 
@@ -40,85 +115,16 @@ class Service extends React.Component{
     console.log('You are interested in: ', nextSelectedTags);
     this.setState({ selectedTags: nextSelectedTags });
   }
+
+
+
+  
   render(){
       //搜索
     const Search = Input.Search;
      //标签
     const { selectedTags } = this.state;
-    const arr=[
-      {
-          id:1,
-          imgpath:require('../assets/images/service1.png'),
-          name:'人事处',
-          bottom:'显示底部内容'
-    },
-    {
-        id:2,
-        imgpath:require('../assets/images/service2.png'),
-        name:'保卫处',
-        bottom:'显示底部内容显示底部内容显示底部内容显示底部内容'
-    },
-    {
-        id:3,
-        imgpath:require('../assets/images/service3.png'),
-        name:'就业管理处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:4,
-        imgpath:require('../assets/images/service4.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:5,
-        imgpath:require('../assets/images/service5.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:6,
-        imgpath:require('../assets/images/service1.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    }
-    ,  {
-        id:7,
-        imgpath:require('../assets/images/service2.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:8,
-        imgpath:require('../assets/images/service3.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:9,
-        imgpath:require('../assets/images/service4.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:10,
-        imgpath:require('../assets/images/service5.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    }
-    ,  {
-        id:11,
-        imgpath:require('../assets/images/service1.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    },
-    {
-        id:12,
-        imgpath:require('../assets/images/service2.png'),
-        name:'行政处',
-        bottom:'显示底部内容'
-    }
-]
+    
 
       return (
 
@@ -150,7 +156,7 @@ class Service extends React.Component{
       </div>
 
               <div className={styles.zmenu}>
-                  <Servicelist selectedTags={this.state.selectedTags}  list={arr} />
+                  <Servicelist current={this.state.current} selectedTags={this.state.selectedTags}  list={arr} />
               </div>
               <div className={styles.pagination}>
                   
