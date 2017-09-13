3240864cddea446cdaff44edf0d2bc328e4826df
Wang Qiang <296361319@qq.com>
2017-09-13 11:36:24 +0800

    服务中心更改 分页

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -15,96 +15,126 @@ const arr=[
         id:1,
         imgpath:require('../assets/images/service1.png'),
         name:'人事处',
-        bottom:'显示底部内容'
+        bottom:'显示底部内容1'
   },
   {
       id:2,
       imgpath:require('../assets/images/service2.png'),
       name:'保卫处',
-      bottom:'显示底部内容显示底部内容显示底部内容显示底部内容'
+      bottom:'显示底部内容显示底部内容显示底部内容显示底部内容2'
   },
   {
       id:3,
       imgpath:require('../assets/images/service3.png'),
       name:'就业管理处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容3'
   },
   {
       id:4,
       imgpath:require('../assets/images/service4.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容4'
   },
   {
       id:5,
       imgpath:require('../assets/images/service5.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容5'
   },
   {
       id:6,
       imgpath:require('../assets/images/service1.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容6'
   }
   ,  {
       id:7,
       imgpath:require('../assets/images/service2.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容7'
   },
   {
       id:8,
       imgpath:require('../assets/images/service3.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容8'
   },
   {
       id:9,
       imgpath:require('../assets/images/service4.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容9'
   },
   {
       id:10,
       imgpath:require('../assets/images/service5.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容10'
   }
   ,  {
       id:11,
       imgpath:require('../assets/images/service1.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容11'
   },
   {
       id:12,
       imgpath:require('../assets/images/service2.png'),
       name:'行政处',
-      bottom:'显示底部内容'
+      bottom:'显示底部内容12'
   }
 ]
 class Service extends React.Component{
+
   constructor(props){
 
       super(props)
 
       this.state={
-        current: 3, 
-        selectedTags: []
-
+        current: 1, //当前页
+        pageSize:5,//每页条数
+        total:arr.length,//数据总数
+        selectedTags: [],
+        listArry:arr,
+        pagelist:[]
       }
 
 
   }
+
+
+// 数据初始化
+componentWillMount(){
+   let list= this.loadData(this.state.current,this.state.pageSize,this.state.listArry);
+   this.setState({
+    pagelist:list
+   })
+}
+
+
   //分页
 
-  onChange = (page) => {
+  pageChange = (page) => {
     console.log(page);
     this.setState({
       current: page,
     });
+
+    let list= this.loadData(page,this.state.pageSize,this.state.listArry);
+    this.setState({
+        pagelist:list
+    })
   }
+
+//   获取数据
+
+loadData=(current,pageSize,datalist)=>{
+    let offset = (current - 1) * pageSize;  
+    let list=(offset + pageSize >= datalist.length) ? datalist.slice(offset, datalist.length) : datalist.slice(offset, offset + pageSize);
+    // console.log(list)
+    return list;
+ }
+
 //标签
 
   handleChange(tag, checked) {
@@ -114,10 +144,46 @@ class Service extends React.Component{
             selectedTags.filter(t => t !== tag);
     console.log('You are interested in: ', nextSelectedTags);
     this.setState({ selectedTags: nextSelectedTags });
+
+    let list=[];
+
+    for(let i=0;i<arr.length;i++){
+
+        if(this.in_array(arr[i].name,nextSelectedTags)){
+            list.push(arr[i]);
+        }
+        
+    }
+
+    //初始化分页
+    
+    this.setState({
+        listArry:list
+    });
+    console.log(list)
+    console.log(this.state.listArry)
+    this.initPage(list);
   }
 
-
-
+  in_array=(search,array)=>{
+    for(var i in array){
+        if(array[i]==search){
+            return true;
+        }
+    }
+    return false;
+  }
+//初始化分页
+initPage=(datalist)=>{
+//    console.log(111)
+// console.log(this.state.listArry)
+let list= this.loadData(1,this.state.pageSize,datalist);
+this.setState({
+    current:1,
+    total:datalist.length?datalist.length:1,
+    pagelist:list
+})
+}
   
   render(){
       //搜索
@@ -156,11 +222,11 @@ class Service extends React.Component{
       </div>
 
               <div className={styles.zmenu}>
-                  <Servicelist current={this.state.current} selectedTags={this.state.selectedTags}  list={arr} />
+                  <Servicelist current={this.state.current} selectedTags={this.state.selectedTags}  list={this.state.pagelist} />
               </div>
               <div className={styles.pagination}>
                   
-              <Pagination  current={this.state.current} onChange={this.onChange} total={50}
+              <Pagination  current={this.state.current} pageSize={this.state.pageSize} onChange={this.pageChange} total={this.state.total}
               />
               </div>
         </div>
