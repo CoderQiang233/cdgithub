081063d1aa1843c0a5838a949d164c049008be5f
sunyanlong <460549468@qq.com>
2017-09-22 14:58:00 +0800

    teacheritem

--- src/components/home/TeacherItem.js
+++ src/components/home/TeacherItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Row,div} from 'antd';
+import {  Link } from 'react-router';
 import styles from './TeacherItem.less';
 
 function TeacherItem() {
   return (
     <div className={styles.normal}>
-      <div >
-          <a href="javascript" >
+      <div>
+          <a href={"#/matter/TLeave"} >
             <img src={require('../../assets/images/jiaoshi/qjsxbl1.png')}/>
             <span>
                <strong>教职工</strong>
@@ -16,38 +17,65 @@ function TeacherItem() {
       </div>
       <div span={6}>
           <a href="javascript" >
-            <img src={require('../../assets/images/jiaoshi/qjsxbl1.png')}/>
+            <img src={require('../../assets/images/jiaoshi/hkdy.png')}/>
             <span>
-               <strong>教职工</strong>
+               <strong>教职工（子女）</strong>
             </span>
-            <small>请假手续</small>
+            <small>户口复印</small>
           </a>
       </div>
       <div span={6}>
           <a href="javascript" >
-            <img src={require('../../assets/images/jiaoshi/qjsxbl1.png')}/>
+            <img src={require('../../assets/images/jiaoshi/hkjy.png')}/>
             <span>
-               <strong>教职工</strong>
+               <strong>教职工（子女）</strong>
             </span>
-            <small>请假手续</small>
+            <small>户口借用</small>
           </a>
       </div>
       <div span={6}>
           <a href="javascript" >
-            <img src={require('../../assets/images/jiaoshi/qjsxbl1.png')}/>
+            <img src={require('../../assets/images/jiaoshi/sfzhfds.png')}/>
             <span>
-               <strong>教职工</strong>
+               <strong>教职工（子女）</strong>
             </span>
-            <small>请假手续</small>
+            <small>身份证换发、丢失补办</small>
           </a>
       </div>
       <div span={6}>
           <a href="javascript" >
-            <img src={require('../../assets/images/jiaoshi/qjsxbl1.png')}/>
+            <img src={require('../../assets/images/jiaoshi/zhenshen.png')}/>
             <span>
                <strong>教职工</strong>
             </span>
-            <small>请假手续</small>
+            <small>政审（无犯罪记录证明）</small>
+          </a>
+      </div>
+      <div span={6}>
+          <a href="javascript" >
+            <img src={require('../../assets/images/jiaoshi/zzzm.png')}/>
+            <span>
+               <strong>教职工</strong>
+            </span>
+            <small>在职证明办理</small>
+          </a>
+      </div>
+      <div span={6}>
+          <a href="javascript" >
+            <img src={require('../../assets/images/jiaoshi/srzmfcg.png')}/>
+            <span>
+               <strong>教职工</strong>
+            </span>
+            <small>收入证明（非出国）办理</small>
+          </a>
+      </div>
+      <div span={6}>
+          <a href="javascript" >
+            <img src={require('../../assets/images/jiaoshi/gzzbl.png')}/>
+            <span>
+               <strong>教职工</strong>
+            </span>
+            <small>工作证办理</small>
           </a>
       </div>
 
